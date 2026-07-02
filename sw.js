1ab817e2c16e60c1769d910211f421d0f7515e35
factshot63-web <factshot63@gmail.com>
2026-07-02 18:03:22 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'nxl-cache-v1';
+const urlsToCache = [
+  './Nxl.html',
+  './manifest.json',
+  './image.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('firebaseio.com') || event.request.url.includes('googleapis.com')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
